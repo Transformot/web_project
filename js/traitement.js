16a3755b91e18c4d90a491f53554ba035e8c4575
Tolosmoc <54775291+Tolosmoc@users.noreply.github.com>
2023-01-09 13:36:50 +0100

    Update traitement.js
    Première petite tentative de vérification des champs entrés

--- js/traitement.js
+++ js/traitement.js
@@ -1,1 +1,12 @@
+function testInput () {
+    let pseudo = document.formSign.signInPseudonyme.value;
+    //pseudo = pseudo.trim ();
 
+    if ((pseudo == "")) {
+        alert ("Merci de renseigner les champs obligatoires.");
+        return False;
+    }
+    else {
+        return True;
+    }
+}
