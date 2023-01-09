bcec5657a47f71e0bd42aad6459f4a57213f1997
Transformot <pgiengirard@gmail.com>
2023-01-09 13:43:45 +0100

    Ajout des templates à Django

--- web_project/chat/views.py
+++ web_project/chat/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import Template, Context, loader
+from django.urls import reverse
+from .models import *
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
