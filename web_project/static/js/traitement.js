19ec87f1c86e0fda814348abf80fc30dfddea429
Tolosmoc <54775291+Tolosmoc@users.noreply.github.com>
2023-01-10 22:13:58 +0100

    Update traitement.js
    Début de la vérification lors de l'inscription ou la connexion.
    (d'autres verif à ajouter + lien avec django)

--- web_project/static/js/traitement.js
+++ web_project/static/js/traitement.js
@@ -1,14 +1,118 @@
-function testInput () {
-    let pseudo = document.formSign.signInPseudonyme.value;
-    //pseudo = pseudo.trim ();
+// Var
+allowed = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.'.split('');
 
-    if ((pseudo == "")) {
-        alert ("Merci de renseigner les champs obligatoires.");
-        return False;
+// Verification for sign in
+let btnIn = document.querySelector("#btnSignIn");
+
+btnIn.addEventListener('click', signInVerification);
+function signInVerification () {
+    let pseudo = document.getElementById("pseudonymeSignIn").value;
+    let password = document.getElementById("passwordSignIn").value;
+    let verification = true;
+
+    if (pseudo == "") {
+        document.getElementById("pseudonymeSignIn").style.border = "solid 2px red";
+        alert("Merci de renseigner un pseudonyme.");
+        verification = false;
+    }
+    else{
+        document.getElementById("pseudonymeSignIn").style.border = "none";
+    }
+
+    if (password == "") {
+        document.getElementById("passwordSignIn").style.border = "solid 2px red";
+        alert("Merci d'entrer votre un mot de passe.");
+        verification = false;
+    }
+    else if (password.length < 6 || password.length > 20) {
+        document.getElementById("passwordSignIn").style.border = "solid 2px red";
+        alert("Mauvais mot de passe.");
+        verification = false;
+    }
+    else{
+        document.getElementById("passwordSignIn").style.border = "none";
+    }
+
+    if (verification === true)
+    {
+        // [Verification si bonnes données dans la base]
+        alert("Comming soon...");
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+// Verification for sign up
+let btnUp = document.querySelector("#btnSignUp");
+
+btnUp.addEventListener('click', signUpVerification);
+function signUpVerification () {
+    let pseudo = document.getElementById("pseudonymeSignUp").value;
+    let password = document.getElementById("passwordSignUp").value;
+    let passwordVerif = document.getElementById("passwordSignUpVerif").value;
+    let verification = true;
+
+    if (pseudo == "") {
+        document.getElementById("pseudonymeSignUp").style.border = "solid 2px red";
+        alert("Merci de renseigner un pseudonyme.");
+        verification = false;
     }
     else {
-        return True;
+        for (let i=0; i < pseudo.length; i++) {
+            if (!(pseudo[i] in allowed)){
+                alert("Votre pseudonyme ne remplit pas les critères.");
+            }
+        }
+        if ('b'=='a') {
+            alert("hello.")
+        }
+        else {
+            document.getElementById("pseudonymeSignUp").style.border = "none";
+        }
     }
+    if (password == "") {
+        document.getElementById("passwordSignUp").style.border = "solid 2px red";
+        document.getElementById("passwordSignUpVerif").style.border = "solid 2px red";
+        alert("Merci de renseigner un mot de passe.");
+        verification = false;
+    }
+    else if (password.length < 6) {
+        document.getElementById("passwordSignUp").style.border = "solid 2px red";
+        document.getElementById("passwordSignUpVerif").style.border = "solid 2px red";
+        alert("Le mot de passe doit comporter six caractères au minimum.");
+        verification = false;
+    }
+    else{
+        document.getElementById("passwordSignUp").style.border = "none";
+        if (passwordVerif == "") {
+            document.getElementById("passwordSignUpVerif").style.border = "solid 2px red";
+            alert("Merci de confirmer le mot de passe.");
+            verification = false;
+        }
+        else if (password !== passwordVerif) {
+            document.getElementById("passwordSignUpVerif").style.border = "solid 2px red";
+            alert("Merci de renseigner le même mot de passe.");
+            verification = false;
+        }
+        else{
+            document.getElementById("passwordSignUpVerif").style.border = "none";
+        }
+    }
+
+    if (verification === true)
+    {
+        // Verification si bonnes données dans la base
+        alert("Comming soon...");
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+// Notes :
     /*
     // Password verification before send to server
     if ((password.lenght < 6)) {
@@ -27,5 +131,3 @@ function testInput () {
         caracSpé += 1;
     }
     */
-
-}
